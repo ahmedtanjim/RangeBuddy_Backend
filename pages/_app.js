eefb2a839ab223558412a28249777a7e98c33893
ahmedtanjim <86013036+ahmedtanjim@users.noreply.github.com>
2024-02-22 17:52:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "../styles/globals.css"
+import "bootstrap/dist/css/bootstrap.css"
+import "react-toastify/dist/ReactToastify.css"
+
+import Head from "next/head"
+import Header from "../components/Header/Header"
+import { ToastContainer } from "react-toastify"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Range Buddy</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://js.stripe.com/v3/"></script>
+      </Head>
+      <ToastContainer position="top-right" autoClose={5000} />
+      <div className="app">
+        <Header></Header>
+        <Component {...pageProps} />
+        <footer className="footer">
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default MyApp
